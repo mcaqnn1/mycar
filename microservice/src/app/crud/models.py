62fdfd3b59e91c98e0bc5aac7ff9b867960f4da3
Arsen <parahatarsen6@gmail.com>
2026-06-06 03:16:23 +0500

    Удалена папка

--- microservice/src/app/crud/models.py
+++
@@ -1,9 +0,0 @@
-from crud.base import BaseService
-from models.items import Items
-from models.users import Users
-
-class ItemsDAO(BaseService):
-    model = Items
-
-class UsersDAO(BaseService):
-    model = Users
