2de2dce2d8f615c7b85de584bae1ff0428de0cde
parakhatar <parahatarsen6@gmail.com>
2026-06-16 23:32:34 +0500

    Delete microservices/app directory

--- microservices/app/core/celery.py
+++
@@ -1,9 +0,0 @@
-from celery import Celery
-from core.settings import settings
-
-celery = Celery(
-    "tasks",
-    broker=settings.REDIS_URL,
-    backend=settings.REDIS_URL,
-    include=["tasks.task"]
-)
